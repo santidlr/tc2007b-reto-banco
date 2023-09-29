90553534b873e7e7f0a0beb16be93f6cb5ab7897
Santiago De Lira Robles <santi.dlr.rob@gmail.com>
2023-09-28 21:30:28 -0600

    Initial Commit

---
+++ TC2007BRetoBanco/TC2007BRetoBancoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TC2007BRetoBancoApp.swift
+//  TC2007BRetoBanco
+//
+//  Created by Santiago De Lira Robles on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TC2007BRetoBancoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
