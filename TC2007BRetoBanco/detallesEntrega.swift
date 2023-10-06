f1e0ac8e81680f5916693dec30c63cf70379fe89
daydr3aming <rogelioguzman73@gmail.com>
2023-10-06 15:23:37 -0500

    Tomar lista en entrega casi finalizado

--- TC2007BRetoBanco/detallesEntrega.swift
+++ TC2007BRetoBanco/detallesEntrega.swift
@@ -8,24 +8,68 @@
 import SwiftUI
 import Firebase
 
-
-
 struct detallesEntrega: View {
     let delivery: Delivery
     let db = Firestore.firestore()
     let bundleRef: DocumentReference
-    
     
     @State private var users: [User] = []
     
     init(delivery: Delivery){
         self.delivery = delivery
         self.bundleRef = db.collection("userbundle").document(delivery.relatedUsers)
-        
-
     }
     
+    var body: some View {
+        VStack {
+            Text("Detalles entrega")
+                .font(.largeTitle)
+                .padding(.bottom)
+
+            
+            // Display delivery details here (e.g., delivery.direction, delivery.date, etc.)
+            
+            HStack {
+                Text("Nombre")
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                    .padding(.trailing, 40) // Adjust spacing as needed
+                Text("Asistencia")
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+            }
+            
+            List {
+                ForEach(users, id: \.id) { user in
+                        HStack(alignment: .center, spacing: 65){
+                                HStack(alignment: .center, spacing: 50){
+                                    Text(user.firstName)
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                    + Text(" ")
+                                    + Text(user.lastName)
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                }
+                                VStack{
+                                    Text("Asistio?")
+                                }
+                        }
+                }
+            }
+            .scrollContentBackground(.hidden)
+            .frame(width: 250)
+            .onAppear {
+                fetchUserData()
+            }
+            .foregroundColor(.orange)
+        }
+    }
+    
+    // MARK: - Fetch Functions
+    
     private func fetchUserData() {
+        users.removeAll()
         bundleRef.getDocument { snapshot, error in
             if let error = error {
                 print("Error fetching bundle document: \(error)")
@@ -33,11 +77,8 @@ struct detallesEntrega: View {
             }
             
             if let data = snapshot?.data(),
-               let userIDs = data["users"] as? [String] { // Assuming your array field is named "userIDs"
-                
-                // Iterate through the user IDs and fetch user data
+               let userIDs = data["users"] as? [String] {
                 for userID in userIDs {
-                    // Assuming you have a function to fetch user data by ID
                     fetchUserByID(userID)
                 }
             }
@@ -45,8 +86,6 @@ struct detallesEntrega: View {
     }
     
     private func fetchUserByID(_ userID: String) {
-        // Use your function to fetch user data by ID here and append it to the users array
-        // Example: Fetch user data from Firestore and append to users array
         db.collection("users").document(userID).getDocument { snapshot, error in
             if let error = error {
                 print("Error fetching user document: \(error)")
@@ -57,45 +96,15 @@ struct detallesEntrega: View {
                let firstname = data["first"] as? String,
                let lastname = data["last"] as? String,
                let born = data["born"] as? Int {
-                
                 let user = User(id: id, firstName: firstname, lastName: lastname, born: born)
                 self.users.append(user)
-                
                 print("User appended: \(user)")
             }
         }
     }
-    
-    var body: some View {
-        VStack {
-            Text("Delivery Details")
-                .font(.largeTitle)
-                .padding()
-            
-            // Display delivery details here (e.g., delivery.direction, delivery.date, etc.)
-            
-            Text("Users in Delivery:")
-                .font(.headline)
-                .padding(.top)
-            
-            List {
-                ForEach(users, id: \.id) { user in
-                    HStack {
-                        Text("Nombre: \(user.firstName)")
-                            .font(.subheadline)
-                        Text("Apellido: \(user.lastName)")
-                            .font(.subheadline)
-                        Text("Nacimiento: \(user.born)")
-                            .font(.subheadline)
-                    }
-                }
-            }
-            .onAppear{
-                fetchUserData()
-            }
-        }
-    }
 }
+
+
 
 
 
