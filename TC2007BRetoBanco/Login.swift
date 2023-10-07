a87f8382d23750a133ca4b0326a7d7bf24d365b5
Santiago De Lira Robles <santi.dlr.rob@gmail.com>
2023-10-06 20:19:41 -0600

    Login funcional

--- TC2007BRetoBanco/Login.swift
+++ TC2007BRetoBanco/Login.swift
@@ -6,13 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct Login: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var userIsLoggedIn = false
     
     
     var body: some View {
+        if userIsLoggedIn {
+            listaBeneficiarios()
+        } else {
+            content
+        }
+    }
+    
+    var content: some View {
         ZStack{
             Color.white
             
@@ -30,69 +40,88 @@ struct Login: View {
                     .frame(width: 300, height: 170, alignment: .center)
                     .cornerRadius(5.0)
                     .shadow(radius: 10.0)
-                    .offset(y: -200)
-                    
+                    .offset(y: -180)
+                
                 Text("Bienvenido")
                     .foregroundColor(.white)
                     .font(.system(size: 40, weight: .bold, design: .rounded))
                     .frame(alignment: .center)
-                    .offset(x: -80, y: -180)
+                    .offset(x: -80, y: -160)
                 
-                TextField("Email", text: $email, prompt: Text("Email").foregroundColor(.gray))
-                
-//                TextField("Email", text: $email)
-//                    .foregroundColor(.white)
-//                    .textFieldStyle(.plain)
-//                    .textoPlaceholder(when: email.isEmpty, alignment: Alignment = .center, placeholder: () -> Text("Email")
-//                                                                  .foregroundColor(.white)
-//                                                                  .bold())
-//                    .textoPlaceholder(when: email.isEmpty) {
-//                        Text("Email")
-//                            .foregroundColor(.white)
-//                            .bold()
-//                    }
-//                    .frame(width: 350, height: 50, alignment: .center)
-//                    .offset(y: -100)
+                TextField("Email", text: $email, prompt: Text("Email").foregroundColor(.gray).bold())
+                    .foregroundColor(.gray)
+                    .textFieldStyle(.plain)
+                    .offset(y:-80)
                 
                 Rectangle()
                     .frame(width: 350, height: 1)
                     .foregroundColor(.white)
+                    .offset(y:-80)
                 
-//                SecureField("Password", text: $password)
-//                    .foregroundColor(.white)
-//                    .textFieldStyle(.plain)
-//                    .placeholder(when: password.isEmpty) {
-//                        Text("Password")
-//                            .foregroundColor(.white)
-//                            .bold()
-//                    }
+                SecureField("Password", text: $password, prompt: Text("Password").foregroundColor(.gray).bold())
+                    .foregroundColor(.gray)
+                    .textFieldStyle(.plain)
+                    .offset(y:-80)
                 
                 Rectangle()
                     .frame(width: 350, height: 1)
                     .foregroundColor(.white)
+                    .offset(y:-80)
                 
-//                Button {
-//                    //sign up
-//                } label: {
-//                    Text("Sign up")
-//                        .bold()
-//                        .frame(width: 200, height: 40)
-//                        .background(RoundedRectangle(cornerRadius: 10, style: .continuous)
-//                            .fill(.linearGradint(color: [.pink, .red], startPoint: .top, endPoint: .bottomTrailing))
-//                        )
-//                        .foregroundColor(.white)
-            }
-            .padding()
+                Button {
+                    register()
+                } label: {
+                    Text("Sign up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.linearGradient(colors: [.pink, .red], startPoint: .top, endPoint: .bottomTrailing))
+                        )
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 110)
                 
                 Button {
                     // login
                 } label: {
-                    Text("Already have an account? Login")
+                    Text("Already have an account? Log in")
+                        .bold()
+                        .foregroundStyle(.red)
                 }
-            
+                .padding(.top)
+                .offset(y: 110)
+            }
+            .frame(width: 350)
+            .onAppear {
+                Auth.auth().addStateDidChangeListener { auth, user in
+                    if user != nil{
+                        userIsLoggedIn.toggle()
+                    }
+                }
+            }
         }
         .ignoresSafeArea()
     }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if error != nil{
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
+    
 }
     
 //extension View {
